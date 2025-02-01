297728d9ccd78fcd2f874a9267eec5e0ba84c500
DHANUSHKUMAR T <t.dhanu2216@gmail.com>
2025-02-01 16:31:51 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,20 +2,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# Load the dataset
+
 df = pd.read_csv('heart_disease_data.csv')
 
-# Handle missing values (if any)
+
 df.fillna(df.mean(), inplace=True)
 
-# Split data into features (X) and target (y)
+
 X = df.drop('target', axis=1)
 y = df['target']
 
-# Split the data into training and test sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize the features
+
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
